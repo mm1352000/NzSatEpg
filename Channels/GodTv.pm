f2c53743654f9ee8cfba053d54f7c35e9e487fac
mm1352000 <music_man1352000@yahoo.com.au>
2014-01-10 20:13:22 +1300

    Fix God TV channel.

--- Channels/GodTv.pm
+++ Channels/GodTv.pm
@@ -26,66 +26,44 @@ sub getChannelData {
 
     print $::dbg "God TV: Starting data grab...\n";
 
-    # Get the seven day schedule. The schedule is provided in the AEST timezone (UTC+10). There is
-    # no way of controlling which 7 days we get so all we can do is work with what we're given.
-    my $response = $::wua->get('http://www.god.tv/weeklyschedule?region=123');
+    # Get the current schedule. The schedule is provided in the AEST timezone (UTC+10). There is no
+    # way of controlling which days we get so all we can do is work with what we're given.
+    my $response = $::wua->get('http://www.god.tv/schedule/api/view/123');
     if ($response->is_error()) {
         print $::dbg 'God TV (' . __LINE__ . '): The request for the schedule HTML failed. ' . $response->status_line() . "\n";
         return undef;
     }
-    my $html = $response->content;
+    my $json = decode_json($response->content);
 
-    # Set up reference dates based on the date of the first day in the schedule.
-    if ($response->content !~ m/.*?class="full_sched_date">.*?(\d+)\s*(.+?)\s*<\/div>\s*(.+)/s) {
-        print $::dbg 'God TV (' . __LINE__ . "): The schedule format has changed.\n";
-        return undef;
-    }
-    my $currentAestDate = DateTime->now(time_zone => 'Australia/Sydney')->set(month => $::monthMap{$2}, day => $1);
-
-    # Throw in a dummy programme on the end of the schedule to save having to do special stuff to
-    # catch the (real) last programme of the week.
-    $html = "$3 class=\"full_sched_start\">06:00 class=\"full_sched_title\">Dummy<\/div> class=\"full_sched_description\"><\/div>";
-
+    # For each day...
     my %schedule = ();
-	my %prevProg = (
-        'title' => ''
-        , 'start' => undef
-        , 'description' => ''
-    );
+    foreach my $k (keys $json->{'data'}) {
+        # Not data for a day.
+        next if ($k !~ m/^\d+$/);
+        
+        my $dayData = $json->{'data'}{$k};
+        foreach my $p (@$dayData) {
+            next if ($p->{'starttstamp'} eq '');
 
-    # For each programme...
-    while ($html =~ m/.*?class="full_sched_start">\s*(\d+):(\d+).+?class="full_sched_title">\s*(.*?)\s*<\/div>.+?class="full_sched_description">\s*(.*?)\s*<\/div>(.*)/s) {
-        my ($progStartHours, $progStartMinutes, $progTitle, $progDesc) = ($1, $2, $3, $4 || '');
-        $html = $5;
-
-        # The guide starts at 6 AM rather than midnight, which means we have to catch the midnight
-        # crossings in order to keep the date correct.
-        if ($currentAestDate->hour > $progStartHours) {
-            $currentAestDate->add(days => 1);
+            my $startTime = DateTime->from_epoch(epoch => $p->{'starttstamp'})->set_time_zone($::targetTimeZone);
+            if ($startTime < $::endDate) {
+                my $endTime = DateTime->from_epoch(epoch => $p->{'endtstamp'})->set_time_zone($::targetTimeZone);
+                if ($endTime > $::startDate) {
+                    my $prog = {
+                        'title' => $p->{'title'}
+                        , 'start' => $startTime->strftime("%Y%m%d%H%M")
+                        , 'end' => $endTime->strftime("%Y%m%d%H%M")
+                    };
+                    if ($p->{'speakers'} ne '') {
+                        $prog->{'description'} = $p->{'speakers'};
+                        if ($p->{'speakers'} ne 'live' && $p->{'speakers'} !~ m/[-:]/) {
+                            $prog->{'host'} = [$p->{'speakers'}];
+                        }
+                    }
+                    $schedule{$prog->{'start'}} = $prog;
+                }
+            }
         }
-        $currentAestDate->set(hour => $progStartHours, minute => $progStartMinutes);
-
-        if ($progTitle eq $prevProg{'title'} && $progDesc eq $prevProg{'description'}) {
-            next;
-        }
-
-        # Add the previous programme to the schedule if shows within the window of interest.
-        my $currentTargetTzDateTime = $currentAestDate->clone()->set_time_zone($::targetTimeZone);
-        if (defined $prevProg{'start'} && $currentTargetTzDateTime > $::startDate && $prevProg{'start'} < $::endDate) {
-            my $progStartString = $prevProg{'start'}->strftime('%Y%m%d%H%M');
-            $schedule{$progStartString} = {
-                'start' => $progStartString
-                , 'end' => $currentTargetTzDateTime->strftime('%Y%m%d%H%M')
-                , 'title' => $prevProg{'title'}
-                , 'description' => $prevProg{'description'}
-            };
-        }
-
-        %prevProg = (
-            'title' => $progTitle
-            , 'start' => $currentTargetTzDateTime
-            , 'description' => $progDesc
-        );
     }
 
     return [
